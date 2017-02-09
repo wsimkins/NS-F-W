9a3fd94f2f55f1089e398740b2f0e03d3b5d708c
Natalie Gray <nataliegray@cs.uchicago.edu>
2017-02-09 13:45:18 -0600

    better test map 8x8

--- test_map.py
+++ test_map.py
@@ -1,14 +1,26 @@
 import pandas as pd
+import numpy as np
 import random
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.DataFrame()
+def draw_heatmap(dataframe):
+	# df = pd.DataFrame(np.random.randn(8,8))
+	sns.heatmap(dataframe, annot=False, fmt="d")
+	sns.plt.show()
 
-df['x'] = random.sample(range(1, 100), 2)
-df['y'] = random.sample(range(1, 100), 2)
+def example_map():
+	df = np.array([[ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.],
+       [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.],
+       [ 0.,  0.,  0.,  0.,  0.,  1.,  1.,  0.],
+       [ 0.,  0.,  0.,  0.,  1.,  0.,  0.,  1.],
+       [ 0.,  2.,  0.,  2.,  1.,  0.,  0.,  1.],
+       [ 1.,  0.,  1.,  0.,  0.,  0.,  0.,  0.],
+       [ 1.,  1.,  1.,  1.,  1.,  1.,  1.,  1.],
+       [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.]])
+	sns.heatmap(df, annot=False, fmt="d")
+	sns.plt.show()
 
-print(df)
 
-sns.heatmap([df.y, df.x], annot=False, fmt="d")
-sns.plt.show()
+
+
