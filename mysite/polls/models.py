191e2e6b33043f470ae1082c6d754f1ff18ad531
Natalie Gray <nataliegray@cs.uchicago.edu>
2017-02-22 17:51:21 -0600

    django tutorial

---
+++ mysite/polls/models.py
@@ -0,0 +1,15 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Question(models.Model):
+	question_text = models.CharField(max_length=200)
+	pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+	# each choice is related to a single question
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	choice_text = models.CharField(max_length=200)
+	votes = models.IntegerField(default=0)
+
+# Create your models here.
